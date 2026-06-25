78b6c723b9b75dc074808409e6b695080e8b74e2
Naina Kesarwani <nainakesarwani5@gmail.com>
2026-06-25 12:03:49 +0530

    added test route

--- server/server.js
+++ server/server.js
@@ -4,14 +4,19 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import inquiryRoutes from "./routes/inquiryRoutes.js";
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
@@ -23,3 +28,5 @@ app.use("/api/inquiries", inquiryRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
